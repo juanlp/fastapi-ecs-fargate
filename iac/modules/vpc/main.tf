4f33fec5c3df91780d62e184f4ebfea2379c2469
Jakub Spórna <jakub.sporna@gmail.com>
2021-12-30 22:11:17 +0100

    applied tf fmt

--- iac/modules/vpc/main.tf
+++ iac/modules/vpc/main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "vpc" {
-  cidr_block            = var.cidr_block
-  enable_dns_hostnames  = true
-  enable_dns_support    = true
+  cidr_block           = var.cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     Name = var.name
